24b50a83c4c057d657bd841427993ca849240101
irfanfy <irfanfy@CATR4>
2022-12-23 18:45:37 +0400

    Add project files.

---
+++ ConsoleApp1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("even number");
+            }
+            else
+            {
+                Console.WriteLine("negative number");
+
+            }
+        }
+    }
+}
